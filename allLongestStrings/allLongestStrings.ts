fdaaab29648758bdef131d3efa26041a506f6e82
guillsav <guillsav@gmail.com>
2019-09-18 12:27:33 -0400

    allLongestStrings done

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,21 @@
 function allLongestStrings(inputArray: string[]): string[] {
+  let strLength = inputArray[0].length;
+  let result = [];
 
+  inputArray.forEach((str: string )=> {
+    if(str.length === strLength) {
+      return result.push(str);
+    };
+
+    if(str.length > strLength) {
+      strLength = str.length;
+      result = [];
+      return result.push(str);
+    };
+
+    return '';
+  })
+  return result;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
