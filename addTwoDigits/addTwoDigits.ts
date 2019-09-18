17d5da6525e7d21847f0ae97287e6db290091d00
guillsav <guillsav@gmail.com>
2019-09-18 11:30:17 -0400

    addTwoDigits refactored using chaining

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,6 +1,5 @@
 function addTwoDigits(n: any): number {
- n = n.toString();
- n.split('');
+ n = n.toString().split('');
  let total = 0;
  for(let i = 0; i < n.length; i++) {
    total += parseInt(n[i]);
